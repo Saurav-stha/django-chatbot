a9f139bb4a31802744a73db99272131ce6fe9f2a
saurav <shresthsaurav8@gmail.com>
2024-06-14 16:17:40 +0545

    feat: error 429

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,8 +1,52 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 
-# Create your views here.
+
+# from openai import 
+
+# client = OpenAI(
+#     # This is the default and can be omitted
+#     api_key=os.environ.get("OPENAI_API_KEY"),
+# )
+
+import openai
+
+openai_api_key = 'sk-proj-THgaxUtNeh7sBGn2czVQT3BlbkFJTmfPAohx00EomhTijwO1'
+openai.api_key = openai_api_key
+
+#have to change model CODE 429(exceed current quota)
+# maybe need premium plan
+def ask_openai(message):
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        # prompt = message,
+        messages = [
+            {
+                'role':'user',
+                'content': message
+            }
+        ],
+        max_tokens = 4096,
+        n=1,
+        stop = None,
+        temperature = 0.7,
+    )
+
+    # print(response)
+    answer = response.choices[0].message['content'].strip()
+
+    return answer
+
 
 def index(request):
+    
     # return HttpResponse('yolooo')
-    return render(request, 'chatbot.html')
+
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = ask_openai(message)
+    
+        context = {'message':message, 'response': response}
+        return JsonResponse(context)
+    
+    return render(request, 'chatbot.html') 
